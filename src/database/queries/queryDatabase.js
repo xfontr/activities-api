298c27a0b5500e76b509579229822712f68df25e
Xifre Font <xifre.font.97@gmail.com>
2022-10-22 18:30:03 +0200

    Created mysql connection and several queries

---
+++ src/database/queries/queryDatabase.js
@@ -0,0 +1,32 @@
+import "../../loadEnvironment.js";
+import Debug from "debug";
+import chalk from "chalk";
+import connection from "./index.js";
+import environment from "../../config/environment.js";
+
+const debug = Debug("activities:queries");
+
+const informationMessages = () => {
+  debug(
+    chalk.yellow(
+      "To visualize the requested query on a table, please do log in at the URL https://remotemysql.com/phpmyadmin/index.php and once there place the SQL query to see the results."
+    )
+  );
+
+  debug(
+    chalk.green(
+      `Username: ${environment.dbUsername} || Password: ${environment.dbPassword}`
+    )
+  );
+};
+
+const queryDatabase = (query) => {
+  connection.query(query, (_, results) => {
+    debug(chalk.blue(`USER REQUESTED QUERY: ${query}`));
+    // eslint-disable-next-line no-console
+    console.log(results);
+    informationMessages();
+  });
+};
+
+export default queryDatabase;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import databaseSync from "./database/index.js";
+import queryDatabase from "./database/queries/queryDatabase.js";
 import startServer from "./server/startServer.js";
+import queries from "./data/queries.js";
 
 (async () => {
   try {
     startServer();
     await databaseSync();
+    queryDatabase(queries.usersWithActivities);
   } catch (error) {
     process.exit(1);
   }
